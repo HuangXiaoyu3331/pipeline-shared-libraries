dfab1625c0c636698e0e49258fcf09efc99104a9
huangxy <1076229832@qq.com>
2021-05-07 01:06:48 +0800

    first commit

---
+++ src/com/hxy/sharelib/repoClone.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/groovy
+
+def call() {
+    closureWrapper(thsi.&action, "gitHub:repoClone")
+}
+
+def action() {
+    checkout([$class           : 'GitSCM',
+              ranches          : [[name: 'main']],
+              userRemoteConfigs: [[url: 'https://github.com/HuangXiaoyu3331/SimpleSprintBootApplication']],
+              changelog        : false,
+              poll             : false])
+}
