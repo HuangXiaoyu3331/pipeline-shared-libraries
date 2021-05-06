3aefd819bf8b62eeb70f32ce681572f50fc9f8e4
huangxy <1076229832@qq.com>
2021-05-07 01:38:00 +0800

    test

--- vars/repoClone.groovy
+++ vars/repoClone.groovy
@@ -1,13 +1,17 @@
 #!/usr/bin/groovy
 
-def call() {
-    closureWrapper(thsi.&action, "gitHub:repoClone")
-}
+//def call() {
+//    closureWrapper(thsi.&action, "gitHub:repoClone")
+//}
+//
+//def action() {
+//    checkout([$class           : 'GitSCM',
+//              ranches          : [[name: 'main']],
+//              userRemoteConfigs: [[url: 'https://github.com/HuangXiaoyu3331/SimpleSprintBootApplication']],
+//              changelog        : false,
+//              poll             : false])
+//}
 
-def action() {
-    checkout([$class           : 'GitSCM',
-              ranches          : [[name: 'main']],
-              userRemoteConfigs: [[url: 'https://github.com/HuangXiaoyu3331/SimpleSprintBootApplication']],
-              changelog        : false,
-              poll             : false])
+def call() {
+    echo "call.............."
 }
